7ac677cce155180835af6dd6d152edbbff346430
Ryan <zhumh.ryan@gmail.com>
2025-05-11 12:24:31 +0800

    bugfix

--- idleMiningEmpire/constants.py
+++ idleMiningEmpire/constants.py
@@ -12,4 +12,4 @@ scaleFactor = 5
 BUILDINGS = [[pygame.transform.scale(pygame.image.load('idleMiningEmpire/assets/elevatorLevel.png'), (elevatorLevelD[0] * scaleFactor, elevatorLevelD[1] * scaleFactor)), 60, 300],
              [pygame.transform.scale(pygame.image.load('idleMiningEmpire/assets/elevatorLevel.png'), (elevatorLevelD[0] * scaleFactor, elevatorLevelD[1] * scaleFactor)), 60, 500],
              [pygame.transform.scale(pygame.image.load('idleMiningEmpire/assets/elevatorLevel.png'), (elevatorLevelD[0] * scaleFactor, elevatorLevelD[1] * scaleFactor)), 60, 700],
-             [pygame.transform.scale(pygame.image.load('idleMiningEmpire/assets/elevatorDock.png'), (elevatorDock[0] * scaleFactor, elevatorDock[1] * scaleFactor)), 60, STAGES[1][0] - (elevatorDock[0]-1) * scaleFactor]]
+             [pygame.transform.scale(pygame.image.load('idleMiningEmpire/assets/elevatorDock.png'), (elevatorDock[0] * scaleFactor, elevatorDock[1] * scaleFactor)), 60, STAGES[0][0] - (elevatorDock[1]-4) * scaleFactor]]
