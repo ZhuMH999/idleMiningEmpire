c0d51a4d06f10418f3c1834061d238987cfed794
Ryan <zhumh.ryan@gmail.com>
2025-05-13 16:56:19 +0800

    moving objects are now a class (0% changed functionality, just cleaner code)

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class View:
                 self.get_text_widget_and_center((0, 0, 0), building[1] + (elevatorShaftScale[0]*elevatorScaleFactor/2), building[2] + (elevatorShaftScale[1]*elevatorScaleFactor/2) - self.model.camy, ELEVATOR_SHAFT_FONT, str(building[3]))
 
         for thing in self.model.moving_things:
-            self.win.blit(thing[0], (thing[3], thing[4] - self.model.camy))
+            self.win.blit(thing.image, (thing.posx, thing.posy - self.model.camy))
 
     def get_text_widget_and_center(self, rgb, c_x, c_y, font, text):
         widget = font.render(text, True, rgb)
@@ -29,14 +29,68 @@ class View:
         rect.center = (c_x, c_y)
         self.win.blit(widget, rect)
 
+class MovingObject:
+    def __init__(self, image, x, y, posx, posy, vel, direction_switch_condition, template_stops, stop_side, manager=False, moving_condition=True):
+        self.image = image
+        self.image_rect = self.image.get_rect(topleft=(posx, posy))
+
+        self.xdir = x
+        self.ydir = y
+        self.posx = posx
+        self.posy = posy
+        self.vel = vel
+
+        self.direction_switch_condition = direction_switch_condition
+        self.template_stops = template_stops
+        self.stops_left = copy.deepcopy(self.template_stops)
+        self.stop_side = stop_side
+        self.manager = manager
+        self.moving_condition = moving_condition
+        self.moving = False
+        self.stop_timer = 0
+
+    def handle_movement(self):
+        if self.moving:
+            if self.stop_timer == 0:
+                if ((self.xdir and self.posx in self.stops_left) or (self.ydir and self.posy in self.stops_left)) and self.stop_side(self.vel):
+                    if self.xdir:
+                        self.stops_left.remove(self.posx)
+                    elif self.ydir:
+                        self.stops_left.remove(self.posy)
+
+                    self.stop_timer = 60
+
+                elif self.direction_switch_condition(self.posx, self.posy):
+                    if not self.manager:
+                        self.moving = False
+
+                    self.vel *= -1
+
+                    if self.xdir:
+                        self.posx += self.vel
+                    elif self.ydir:
+                        self.posy += self.vel
+
+                elif len(self.stops_left) == 0:
+                    self.stops_left = copy.deepcopy(self.template_stops)
+                    self.vel *= -1
+
+                else:
+                    if self.xdir:
+                        self.posx += self.vel
+                    elif self.ydir:
+                        self.posy += self.vel
+
+            else:
+                self.stop_timer -= 1
+
 class Model:
     def __init__(self):
         self.run = True
         self.camy = 0
 
-        # thing, xdir, ydir, posx, posy, vel(x or y), return true if switch direction (x or y), waiting time, stops left in cycle, template stops, only stop which side, unlocked manager, moving, moving condition (do later)
-        self.moving_things = [[elevator, False, True, 70, 320, 2, lambda x, y: y < 320, 0, [int(300 + 200 * (i+1) + 30) for i in range(9)], [int(300 + 200 * (i+1) + 30) for i in range(9)], lambda vel: vel > 0, False, False],
-                              [cart, True, False, 500, 258, -2, lambda x, y: x > 500, 0, [184], [184], lambda vel: vel < 0, False, False]]
+        self.moving_things = [MovingObject(elevator, False, True, 70, 320, 2, lambda x, y: y < 320, [int(300 + 200 * (i+1) + 30) for i in range(9)], lambda vel: vel > 0),
+                              MovingObject(cart, True, False, 500, 258, -2, lambda x, y: x > 500, [184], lambda vel: vel < 0)]
 
     def handle_keypress(self, key):
         if key == 0 and self.camy != 0:
@@ -48,51 +102,12 @@ class Model:
 
     def handle_mouseclick(self, x, y):
         for i in range(len(self.moving_things)):
-            if not self.moving_things[i][11]:
-                if i == 0:
-                    if elevator.get_rect(center=(self.moving_things[0][3] + (elevatorScale[0] * elevatorScaleFactor) / 2, self.moving_things[0][4] + (elevatorScale[1] * elevatorScaleFactor) / 2 - self.camy)).collidepoint((x, y)) and not self.moving_things[i][12]:
-                        self.moving_things[i][12] = True
-                elif i == 1:
-                    if cart.get_rect(center=(self.moving_things[1][3] + (cartScale[0] * cartScaleFactor) / 2, self.moving_things[1][4] + (cartScale[1] * cartScaleFactor) / 2 - self.camy)).collidepoint((x, y)) and not self.moving_things[i][12]:
-                        self.moving_things[i][12] = True
+            if self.moving_things[i].image_rect.collidepoint((x, y)) and not self.moving_things[i].moving:
+                self.moving_things[i].moving = True
 
     def handle_movement(self):
         for thing in self.moving_things:
-            if thing[12]:
-                if thing[7] == 0:
-                    if ((thing[1] and thing[3] in thing[8]) or (thing[2] and thing[4] in thing[8])) and thing[10](thing[5]):
-                        if thing[1]:
-                            thing[8].remove(thing[3])
-                        elif thing[2]:
-                            thing[8].remove(thing[4])
-                        thing[7] = 60
-
-                    elif thing[6](thing[3], thing[4]):
-                        if not thing[11]:
-                            thing[12] = False
-
-                        thing[5] *= -1
-                        if thing[1]:
-                            thing[3] += thing[5]
-                        elif thing[2]:
-                            thing[4] += thing[5]
-
-                    elif len(thing[8]) == 0:
-                        thing[8] = copy.deepcopy(thing[9])
-                        thing[5] *= -1
-                        if thing[1]:
-                            thing[3] += thing[5]
-                        elif thing[2]:
-                            thing[4] += thing[5]
-
-                    else:
-                        if thing[1]:
-                            thing[3] += thing[5]
-                        elif thing[2]:
-                            thing[4] += thing[5]
-
-                else:
-                    thing[7] -= 1
+            thing.handle_movement()
 
 class Controller:
     def __init__(self):
